c84ae8196009cfcb7625372aafcb3227f66665a7
Ajay <ajayvishwakarma457@gmail.com>
2025-11-09 21:52:01 +0530

    sacling pending

--- main.tf
+++ main.tf
@@ -16,6 +16,58 @@ module "iam" {
   common_tags  = var.common_tags
 }
 
+# ------------------------------------
+# SECURITY GROUPS (Reusable Modules)
+# ------------------------------------
+
+# ALB Security Group
+module "alb_sg" {
+  source       = "./modules/security-group"
+  project_name = var.project_name
+  vpc_id       = module.vpc.vpc_id
+  sg_name      = "alb-sg"
+  common_tags  = var.common_tags
+  description  = "Security group for Application Load Balancer"
+
+  ingress_rules = [
+    {
+      from_port   = 80
+      to_port     = 80
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+      description = "HTTP access"
+    },
+    {
+      from_port   = 443
+      to_port     = 443
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+      description = "HTTPS access"
+    }
+  ]
+}
+
+# APP Security Group
+module "app_sg" {
+  source       = "./modules/security-group"
+  project_name = var.project_name
+  vpc_id       = module.vpc.vpc_id
+  sg_name      = "app-sg"
+  common_tags  = var.common_tags
+  description  = "Security group for App instances"
+
+  ingress_rules = [
+    {
+      from_port       = 80
+      to_port         = 80
+      protocol        = "tcp"
+      cidr_blocks     = ["0.0.0.0/0"] # or restrict to ALB SG later
+      description     = "Allow HTTP from ALB"
+    }
+  ]
+}
+
+# WEB Security Group
 module "web_sg" {
   source       = "./modules/security-group"
   project_name = var.project_name
@@ -70,4 +122,32 @@ module "dynamodb" {
   sort_key      = "createdAt"
   sort_key_type = "S"
 }
+
+# module "scaling" {
+#   source = "./modules/scaling"
+
+#   project_name       = var.project_name
+#   common_tags        = var.common_tags
+#   vpc_id             = module.vpc.vpc_id
+#   public_subnet_ids  = module.vpc.public_subnet_ids
+#   private_subnet_ids = module.vpc.private_subnet_ids
+
+#   # Security Groups (from SG modules)
+#   alb_sg_id = module.alb_sg.security_group_id
+#   app_sg_id = module.app_sg.security_group_id
+
+#   # Compute
+#   instance_type             = "t3.micro"
+#   iam_instance_profile_name = "${var.project_name}-ec2-profile"
+
+#   # Capacity
+#   desired_capacity = 2
+#   min_size         = 2
+#   max_size         = 4
+
+#   # App
+#   health_check_path  = "/"
+#   alb_certificate_arn = module.acm.alb_cert_arn
+# }
+
 
