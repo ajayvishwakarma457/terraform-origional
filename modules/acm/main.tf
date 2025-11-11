ba663999ab4f58f1e0994c53eb8ff9a91b42b6e7
Ajay <ajayvishwakarma457@gmail.com>
2025-11-11 21:40:29 +0530

    acm removed provider code

--- modules/acm/main.tf
+++ modules/acm/main.tf
@@ -2,14 +2,14 @@
 # MODULE: ACM Certificates (ALB + CloudFront)
 ##############################################
 
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
+# terraform {
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "~> 5.0"
+#     }
+#   }
+# }
 
 # ---------- Providers ----------
 provider "aws" {
